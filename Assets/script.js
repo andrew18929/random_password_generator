9afe0646b99c50d187a7f7e0dbe79cfb5b82c470
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-08-14 16:03:46 -0500

    deleted function generators and added arrays

--- Assets/script.js
+++ Assets/script.js
@@ -6,27 +6,11 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 
-//functions to generate random characters
-function getRandomLowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUppercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-}
-
-var lowerCaseLetters = getRandomLowercase();
-var upperCaseLetters = getRandomUppercase();
-var numbers = getRandomNumber();
-var specialCharacters = getRandomSymbol();
+//characters that i want used in the password
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "'", "/", ",", ".", "<", ">"];
 
 
 //empty arrays to generate from
@@ -36,7 +20,7 @@ var userChoices = [];
 //generatePassword function
 function generatePassword() {
   var numberOfCharacters = window.prompt("How many number of characters to include (Password should be minimum 8 characters and maximum 128 characters):");
-}
+
 
 //check to see if number of characters is correct
 if (numberOfCharacters >= 8 && numberOfCharacters <= 128) {
@@ -46,33 +30,60 @@ if (numberOfCharacters >= 8 && numberOfCharacters <= 128) {
   generatePassword();
 }
 
+
 //user selected character prompts
 function userSelectedCharacters() {
   var lowerCaseSelection = window.confirm("Do you want lowercase letters used in your password?");
   if (lowerCaseSelection === true) {
     userChoices = userChoices.concat(lowerCaseLetters);
-    passwordArray.push(lowerCaseLetters);
+    passwordArray.push(lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]);
   }
 
   var upperCaseSelection = window.confirm("Do you want uppercase letters used in your password?");
   if (upperCaseSelection === true) {
     userChoices = userChoices.concat(upperCaseLetters);
-    passwordArray.push(upperCaseLetters);
+    passwordArray.push(upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)]);
   }
 
   var numbersSelection = window.confirm("Do you want numbers used in your password?");
   if (numbersSelection === true) {
     userChoices = userChoices.concat(numbers);
-    passwordArray.push(numbers);
+    passwordArray.push(numbers[Math.floor(Math.random() * numbers.length)]);
   }
 
   var symbolsSelection = window.confirm("Do you want symbols used in your password?");
-  if (lowerCaseSelection === true) {
+  if (symbolsSelection === true) {
     userChoices = userChoices.concat(specialCharacters);
-    passwordArray.push(specialCharacters);
+    passwordArray.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
   }
+
+  if (!lowerCaseSelection && !upperCaseSelection && !numbersSelection && !symbolsSelection) {
+    alert("You should include at least one character type.");
+    userSelectedCharacters();
+  }
+
+  console.log(userChoices);
+  console.log(passwordArray);
+
+  var passwordArrayLength = passwordArray.length;
+
+  for (i = passwordArrayLength; i < numberOfCharacters; i++) {
+    passwordArray.push (userChoices[Math.floor(Math.random() * userChoices.length)]);
+  }
+
+  var finalPassword = ""
+  for(f = 0; f < numberOfCharacters; f++) {
+    finalPassword = finalPassword + passwordArray[Math.floor(Math.random() * passwordArray.length)]
+  }
+
+  console.log(finalPassword);
+  return finalPassword;
+
 }
 
+return password;
+console.log(password);
+}
 
 
 
